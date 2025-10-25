2a966d8538634b92db28ee9a8c0db147db359abf
danijelaaaaaaaaa <danijela10slavujevic@gmail.com>
2025-10-25 08:13:55 +0200

    Before defense

--- src/main/java/com/example/bookproject/repositories/GenreRepository.java
+++ src/main/java/com/example/bookproject/repositories/GenreRepository.java
@@ -1,10 +1,10 @@
 package com.example.bookproject.repositories;
 
 
-import com.example.bookproject.dto.GenreDTO;
+
 import com.example.bookproject.models.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
 
 import java.util.List;
 
